697d799216ae0fcb49ddb2c584c64619f8906ab8
Pedro Alejandro Lima Dos Anjos <alejandropedro548@gmail.com>
2025-11-18 14:14:11 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,17 +8,31 @@ def index():
     return render_template("index.html")
 
 #Primeira Rota é soma
-
+@app.route("/soma")
+def soma():
+    v1 = float (request.args.get("valor1",0))
+    v2 = float (request.args.get("valor2",0))
+    return{"resultado": v1 + v2}
 
 # Segunda é subtrair 
-
+@app.route("/subtrair")
+def subtrair():
+    v1 = float (request.args.get("valor1",0))
+    v2 = float (request.args.get("valor2",0))
+    return{"resultado": v1 - v2}
 
 # Terceira é multiplicar
-
-
-
+@app.route("/multiplicar")
+def multiplicar():
+    v1 = float (request.args.get("valor1",0))
+    v2 = float (request.args.get("valor2",0))
+    return{"resultado": v1 * v2}
 # Terceira é dividir 
-
+@app.route("/dividir")
+def dividir():
+    v1 = float (request.args.get("valor1",0))
+    v2 = float (request.args.get("valor2",0))
+    return{"resultado": v1 / v2}
 
 # 🚨🚨🚨🚨🚨🚨🚨🚨🚨 Não mexa aqui, pois isso que executa o arquivo 🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨🚨
 if __name__ == "__main__":
